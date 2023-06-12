6fd70854c4bb1ccc993115f86a5d529688589f0f
zeemlooser <myhpc0153@gmail.com>
2023-06-12 23:40:39 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ShoppingListPage(),
+    );
+  }
+}
+
+class ShoppingListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shopping List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Cart is empty')),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('Apples'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('Bananas'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('Oranges'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('Milk'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text('Bread'),
+          ),
+        ],
+      ),
+    );
+  }
+}
